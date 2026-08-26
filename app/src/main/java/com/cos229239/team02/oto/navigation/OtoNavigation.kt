ed3839ec4b13a2f66293bf1d31c882f74d005b0d
David Patterson <Dspatterson15@gmail.com>
2026-08-25 21:43:56 -0400

    Created the Explorer Screen

--- app/src/main/java/com/cos229239/team02/oto/navigation/OtoNavigation.kt
+++ app/src/main/java/com/cos229239/team02/oto/navigation/OtoNavigation.kt
@@ -14,6 +14,7 @@ import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import com.cos229239.team02.oto.ui.screens.home.HomeScreen
+import com.cos229239.team02.oto.ui.screens.explorer.ExplorerScreen
 
 @Composable
 fun OtoNavigation() {
@@ -38,9 +39,10 @@ fun OtoNavigation() {
             }
 
             entry<OtoRoute.Explorer> {
-                ModePlaceholder(
-                    title = "Explorer Mode",
-                    backStack = backStack
+                ExplorerScreen(
+                    onBackClick = {
+                        backStack.removeLastOrNull()
+                    }
                 )
             }
 
